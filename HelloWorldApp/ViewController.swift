a8af13ca085d4be1b6b0e2aacc2f9f076f3424ba
Anton <anton.alymov87@gmail.com>
2020-01-28 15:00:35 +0400

    Initial Commit

---
+++ HelloWorldApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by Антон Алымов on 28.01.2020.
+//  Copyright © 2020 Anton Alymov Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
